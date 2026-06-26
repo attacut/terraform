e4c97d9f26555fdab9da08101213bd47242ab1d0
Noppasin <noppasin.sr@gmail.com>
2026-06-26 13:33:12 +0700

    chore: require terraform >= 1.11.0 for use_lockfile support

--- modules/aws/vpc/versions.tf
+++ modules/aws/vpc/versions.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 1.5.0"
+  required_version = ">= 1.11.0"   # ดันขึ้นจาก 1.5.0 เพื่อรองรับ use_lockfile
   required_providers {
     aws = {
       source  = "hashicorp/aws"
